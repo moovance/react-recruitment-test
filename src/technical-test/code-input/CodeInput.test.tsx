ba1046e1b5b369f4f4801ff73c6c8a70c4770046
washtakzo <washtakzo@gmail.com>
2023-02-01 20:42:28 +0100

    add test : displays inputs & call function when full

--- src/technical-test/code-input/CodeInput.test.tsx
+++ src/technical-test/code-input/CodeInput.test.tsx
@@ -1,3 +1,38 @@
-it('works', () => {
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import CodeInput from "./CodeInput";
+
+it("works", () => {
   expect(true).toBe(true);
+});
+
+it("displays inputs on screen, with first one focused", async () => {
+  render(
+    <CodeInput
+      length={4}
+      onCodeFull={(code: string) => {
+        console.log({ code });
+      }}
+    />
+  );
+
+  const inputs: HTMLInputElement[] = await screen.findAllByRole("spinbutton", {
+    name: "",
+  });
+  expect(inputs).toHaveLength(4);
+  expect(inputs[0]).toHaveFocus();
+});
+
+it("calls onCodeFull when all inputs filled", async () => {
+  const onCodeFullHandler = (code: string) => {
+    console.log(code);
+  };
+  const testingCode = "1285";
+  const consoleSpy = jest.spyOn(console, "log");
+
+  render(<CodeInput length={4} onCodeFull={onCodeFullHandler} />);
+
+  userEvent.keyboard(testingCode);
+
+  expect(consoleSpy).toHaveBeenCalledWith(testingCode);
 });
